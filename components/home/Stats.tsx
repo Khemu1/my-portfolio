b3624fda919602d2fc82f45fb7165fdfedfdba46
khemu1 <alghost2004@gmail.com>
2026-01-01 04:09:50 +0200

    style(loader): added for commits

--- components/home/Stats.tsx
+++ components/home/Stats.tsx
@@ -2,16 +2,18 @@
 import { useEffect, useState } from "react";
 import CountUp from "react-countup";
 import { stats as baseStats } from "@/data";
+import { Loader2 } from "lucide-react";
 
 const Stats = () => {
   const [stats, setStats] = useState(baseStats);
+  const [loadingCommits, setLoadingCommits] = useState(false);
 
   useEffect(() => {
     const fetchCommits = async () => {
+      setLoadingCommits(true);
       try {
         const res = await fetch("/api/commits");
         const data = await res.json();
-        console.log(data, "data");
         setStats((prev) =>
           prev.map((stat) =>
             stat.name === "Code Commits"
@@ -21,6 +23,8 @@ const Stats = () => {
         );
       } catch (err) {
         console.error("Failed to load commits:", err);
+      } finally {
+        setLoadingCommits(false);
       }
     };
 
@@ -31,27 +35,36 @@ const Stats = () => {
     <section className="pt-4 pb-12 xl:pt-24 xl:pb-0">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
-          {stats.map((stat, index) => (
-            <div
-              key={index}
-              className="flex gap-4 items-center justify-center xl:justify-start flex-1 bg-surface-dark rounded-xl px-6 py-4"
-            >
-              <CountUp
-                end={stat.value}
-                duration={5}
-                delay={1}
-                decimals={stat.value % 1 !== 0 ? 1 : 0}
-                className="text-4xl xl:text-6xl font-extrabold bg-gradient-to-r from-accent to-secondary bg-clip-text text-transparent"
-              />
-              <p
-                className={`${
-                  stat.name.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-                } text-white/80 leading-snug text-lg`}
+          {stats.map((stat, index) => {
+            const isCommits = stat.name === "Code Commits";
+            return (
+              <div
+                key={index}
+                className="flex gap-4 items-center justify-center xl:justify-between flex-1 bg-surface-dark rounded-xl px-6 py-4"
               >
-                {stat.name}
-              </p>
-            </div>
-          ))}
+                {isCommits && loadingCommits ? (
+                  <span className="text-4xl xl:text-6xl font-extrabold  animate-spin">
+                    <Loader2 className="" />
+                  </span>
+                ) : (
+                  <CountUp
+                    end={stat.value}
+                    duration={5}
+                    delay={1}
+                    decimals={stat.value % 1 !== 0 ? 1 : 0}
+                    className="text-4xl xl:text-6xl font-extrabold bg-gradient-to-r from-accent to-secondary bg-clip-text text-transparent"
+                  />
+                )}
+                <p
+                  className={`${
+                    stat.name.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } text-white/80 leading-snug text-lg`}
+                >
+                  {stat.name}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
--- data/index.tsx
+++ data/index.tsx
@@ -20,7 +20,7 @@ export const stats = [
   { name: "Years of Experience", value: 1 },
   { name: "Free Lancing Projects", value: 3 },
   { name: "Side Projects", value: 62 },
-  { name: "Code Commits", value: 0 },
+  { name: "Code Commits", value: 1800 },
 ];
 
 export const services = [
